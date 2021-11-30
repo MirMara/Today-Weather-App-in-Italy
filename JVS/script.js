2f1f80b20318f1b6c4d3ecd1ce0d84a38306fe37
Miriana Maranzano <mirianamaranzano2@gmail.com>
2021-11-30 18:06:33 +0100

    feat: work in progress/js ok

--- JVS/script.js
+++ JVS/script.js
@@ -16,5 +16,25 @@
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-Bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function (){
+    weather.search();
+});
+
+document.querySelector(".search-Bar").addEventListener("keyup", function (event){
+    if (event.key == "Enter"){
+        weather.search();
     }
-}
+});
